8d6972d4e3e21c539fd17042469f14aefd8eccc3
zvereva <96833638+zvereva-s@users.noreply.github.com>
2022-06-10 16:38:42 +0300

    поправки по замечаниям
    - скролл
    - марджин кнопок add в модалке
    - транзишн на кнопку закрытия
    - закрытие модалки по бекдропу
    - анимация на кнопки

--- src/js/modal.js
+++ src/js/modal.js
@@ -10,15 +10,16 @@ const refs = {
   gallery: document.querySelector('.gallery__list'),
   modalContainer: document.querySelector('.modal__container'),
   galleryItemId: document.querySelector('.gallery__item'),
+  backdrop: document.querySelector('.backdrop'),
 };
 
 refs.gallery.addEventListener('click', onOpenModal);
 refs.closeModalBtn.addEventListener('click', toggleModal);
+refs.backdrop.addEventListener('click', onBackdropClick);
 
 function toggleModal() {
   refs.modal.classList.toggle('is-hidden');
 }
-
 
 async function onOpenModal(e) {
   if (e.target.nodeName === 'UL') {
@@ -28,16 +29,16 @@ async function onOpenModal(e) {
   document.addEventListener('keydown', closeByEsc);
   toggleModal();
 
-    const id = e.target.closest('LI').id;
+  const id = e.target.closest('LI').id;
 
-    try {
-      const { data } = await movieApi.getMovieInfo(id);
-    
-        data.genres = data.genres.map(({ name }) => name).join(', ');
-        data.poster_path = e.target.closest('IMG').src;
+  try {
+    const { data } = await movieApi.getMovieInfo(id);
 
-      refs.modalContainer.innerHTML = "";
-      refs.modalContainer.insertAdjacentHTML('beforeend', fillModalMarkup(data));
+    data.genres = data.genres.map(({ name }) => name).join(', ');
+    data.poster_path = e.target.closest('IMG').src;
+
+    refs.modalContainer.innerHTML = '';
+    refs.modalContainer.insertAdjacentHTML('beforeend', fillModalMarkup(data));
   } catch (err) {
     console.log(err);
   }
@@ -50,4 +51,9 @@ function closeByEsc(e) {
   toggleModal();
   document.removeEventListener('keydown', closeByEsc);
 }
-
+function onBackdropClick(e) {
+    if (e.target !== e.currentTarget) {
+        return;
+    }
+    toggleModal();
+ };
